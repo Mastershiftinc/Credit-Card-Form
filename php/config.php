53eb8c8a7eab19b0523fc6e0ed52ad38e89fb15c
Richardson Ceballo <79226982+serocool17@users.noreply.github.com>
2022-05-07 21:08:31 -0400

    Create config.php

---
+++ php/config.php
@@ -0,0 +1,21 @@
+<?php
+class dbConfing{
+    Protected $serverName;
+    Protected $userName;
+    Protected $password;
+    Protected $dbName;
+    function __construct(){
+    
+    $this -> serverName = 'localhost';
+    $this -> userName = 'your_user';
+    $this -> password = 'your_password';
+    $this -> dbName = 'your_database';
+
+
+
+    }
+}
+
+
+
+?>
